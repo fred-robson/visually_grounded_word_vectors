69cb5972b599bdda18795f9fd9eb68dd8621668f
Fred Robson <frederickwlrobson@gmail.com>
2018-05-23 18:59:41 -0700

    ResNet output is working

--- utils/resnet_output.py
+++ utils/resnet_output.py
@@ -3,35 +3,11 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 from data_utils import CocoCaptions
 import numpy as np
-
-Model = models.resnet101(pretrained = True).eval()
-Model = Model.double()
-
-
-coco = CocoCaptions(3)
-
-for image,image_id in coco.get_all_images():
-	
-
-	image = image.astype(float,copy=False)
-	image = np.moveaxis(image,2,0) #i.e. mini-batches of 3-channel RGB images of shape (3 x H x W)
-	normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
-	image = torch.from_numpy(image)
-	normalize(image)
-	image = image.unsqueeze(0)
-	print(image.shape)
-	output = Model(image)
-
-
-
-
-
+from tqdm import tqdm
 
 
 
 '''
-
 All pre-trained models expect input images normalized in the same way,
 i.e. mini-batches of 3-channel RGB images of shape (3 x H x W), where H and W
 are expected to be at least 224. The images have to be loaded in to a range of [0, 1] 
@@ -40,6 +16,48 @@ You can use the following transform to normalize:
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
 '''
+
+def main(data = 3):
+	'''
+	Creates output for data loaded from coco. If 
+	'''
+
+	Model = models.resnet101(pretrained = True).eval()
+	Model = Model.float()
+
+	save_location = None
+
+	coco = CocoCaptions(data)
+
+	for image,image_id in tqdm(coco.get_all_images(),total=coco.num_images()):
+			
+		trans = transforms.Compose([
+									transforms.ToPILImage(), #RandomCrop requires PIL image
+									transforms.RandomCrop(size=224),
+									transforms.ToTensor(), #Convert back to tensor to normalize
+									transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
+									])
+		
+		image = trans(image)
+		image = image.unsqueeze(0) #unsqueeze bc it needs to be a batch. Here use a batch of size 1
+		output = Model(image)
+		output_np = output.detach().numpy()
+		save_address = coco.get_image_resnet_address(image_id)
+		np.save(save_address,output_np)
+
+
+
+if __name__ == "__main__":
+	
+	main(3)
+	
+	try:  main(2)
+	except: print("Train and Val data has not been loaded")
+
+
+
+
+
 
 
 
