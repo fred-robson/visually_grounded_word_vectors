759e14bf2a4e7a71dbdd1843decd2656e41f3add
Fred Robson <frederickwlrobson@gmail.com>
2018-05-26 15:00:54 -0700

    changed so that it resizes to appropriate size, then takes a crop

--- utils/resnet_output.py
+++ utils/resnet_output.py
@@ -6,6 +6,7 @@ import numpy as np
 from tqdm import tqdm
 
 
+image_size = 224 #Size of smallest dimension
 
 '''
 All pre-trained models expect input images normalized in the same way,
@@ -31,23 +32,24 @@ def main(data = 3):
 
 	for image,image_id in tqdm(coco.get_all_images(),total=coco.num_images()):
 		
-		try:	
-			trans = transforms.Compose([
-										transforms.ToPILImage(), #RandomCrop requires PIL image
-										transforms.RandomCrop(size=224),
-										transforms.ToTensor(), #Convert back to tensor to normalize
-										transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
-										])
-			
-			image = trans(image)
-			image = image.unsqueeze(0) #unsqueeze bc it needs to be a batch. Here use a batch of size 1
-			output = Model(image)
-			output_np = output.detach().numpy()
-			save_address = coco.get_image_resnet_address(image_id)
-			np.save(save_address,output_np)
-		except:
-			print("Error at:",image_id)
-			raise
+		#Convert to PiL image for resizing + croppping 
+		convert_to_pil = transforms.ToPILImage()
+		image = convert_to_pil(image)
+		transforms.functional.resize(image,size=image_size) #model requires everything is at least 
+
+		trans = transforms.Compose([			
+									transforms.RandomCrop(size=image_size),
+									transforms.ToTensor(), #Convert back to tensor to normalize
+									transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
+									])
+		
+		image = trans(image)
+		image = image.unsqueeze(0) #unsqueeze bc it needs to be a batch. Here use a batch of size 1
+		output = Model(image)
+		output_np = output.detach().numpy()
+		save_address = coco.get_image_resnet_address(image_id)
+		np.save(save_address,output_np)
+		
 
 
 
