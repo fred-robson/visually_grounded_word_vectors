887e6d992e85fb9909f321c81f0036ead8bb7751
Loren AC <lkac@stanford.edu>
2018-05-27 16:40:47 -0700

    update git ignore, save progress on overall structure and baseline models

---
+++ hpsearch/hyperband_skopt.py
@@ -0,0 +1,42 @@
+import numpy as np
+import tensorflow as tf
+import keras
+
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import InputLayer, Input
+from keras.layers import Reshape, MaxPooling2D
+from keras.layers import Conv2D, Dense, Flatten
+from keras.callbacks import TensorBoard
+from keras.optimizers import Adam
+from keras.models import load_model
+
+import skopt
+from skopt import gp_minimize, forest_minimize
+from skopt.space import Real, Categorical, Integer
+from skopt.plots import plot_convergence
+from skopt.plots import plot_objective, plot_evaluations
+from skopt.plots import plot_histogram, plot_objective_2D
+from skopt.utils import use_named_args
+
+
+class HPSearcher(object):
+
+    def __init__(self, default_parameters):
+        self.default_parameters = default_parameters
+
+	def _search_space(self):
+		space_learning_rate = Real(low=1e-6, high=1e-2, prior='log-uniform',
+                         name='learning_rate')
+    	space_hidden_dim = Integer(low=5, high=512, name='hidden_dim')
+        space_optimizers = Categorical(categories=['adam','sgd'],name='optimizer')
+    	return [dim_learning_rate, dim_hidden_dim, dim_optimizers]
+
+	def run(self, func):
+		search_result = gp_minimize(func=func,
+                            dimensions=self._search_space,
+                            acq_func='EI', # Expected Improvement.
+                            n_calls=40,
+                            x0=self.default_parameters)
+        return search_result
+
---
+++ models/prototypes/baseline.py
@@ -0,0 +1,66 @@
+import keras
+import tensorflow as tf
+import numpy as np 
+from keras.models import Model 
+from keras.models import Sequential
+from keras.layers import Bidirectional
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import TimeDistributed
+from tensorflow.contrib.training import HParams
+
+
+class Cap2(object):
+
+	def __init__(self, h_params, model_type='cap2all'):
+		self.h_params = HParams(**h_params)
+		self.model_type = model_type
+		self.model = self.build()
+
+	def _encoder(self):
+		model = Sequential()
+		model.add(Bidirectional(LSTM(hidden_dim, dropout=encoder_dropout, recurrent_dropout=encoder_dropout, return_sequences=True)))
+		model.add(TimeDistributed(Dense()))
+		return model
+
+	def _decoder(self, model):
+
+	def _map2latent(self, model):
+
+	def fit(self, x, labels):
+		if self.model_type == 'cap2all':
+			assert (len(labels) == 2),"Not Enough Labels For Cap2All"
+
+	def build(self):
+		model = self._encoder()
+		if self.model_type == 'cap2cap':
+			model = self._decoder(model)
+		if self.mode_type == 'cap2img':
+			model = self._map2latent(model)
+		model.name = self.model_type
+		return model
+
+
+
+
+def get_encoder(hidden_dim, model='all', hidden_dim=20, encoder_dropout=0.0, encoder_recurrent_dropout=0.0):
+	
+	model = Sequential()
+	model.add(Bidirectional(LSTM(hidden_dim, dropout=encoder_dropout, recurrent_dropout=encoder_dropout, return_sequences=True)))
+	model.add(TimeDistributed(Dense()))
+
+	return model
+
+def get_decoder
+
+def cap_2_all(data, hidden_dim=20, encoder_dropout=0.0, encoder_recurrent_dropout=0.0):
+	input_cap, label_cap, label_img = data
+def cap_2_cap(data, hidden_dim=20, encoder_dropout=0.0, encoder_recurrent_dropout=0.0):
+	input_cap, label_cap = data
+	model = encoder(hidden_dim, dropout=encoder_dropout, recurrent_dropout=encoder_dropout, return_sequences=True)
+def cap_2_img(data, hidden_dim=20, encoder_dropout=0.0, encoder_recurrent_dropout=0.0):
+	input_cap, label_img = data
+	model = Sequential()
+	model.add(Bidirectional(LSTM(hidden_dim, dropout=encoder_dropout, recurrent_dropout=encoder_dropout, return_sequences=True)))
+	model.add(TimeDistributed(Dense()))
+
