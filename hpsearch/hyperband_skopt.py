583ceea1551be7159d45e5f01504e1f417a65951
Fred Robson <frederickwlrobson@gmail.com>
2018-06-03 21:33:27 -0700

    changed bat ch size again

--- hpsearch/hyperband_skopt.py
+++ hpsearch/hyperband_skopt.py
@@ -175,7 +175,7 @@ class HPSearcher(object):
                     history = model.fit(inputs,
                                     outputs,
                                     epochs=10,
-                                    batch_size=512,
+                                    batch_size=256,
                                     validation_split=0.2,
                                     validation_data=validation_data,
                                     callbacks=callbacks)
